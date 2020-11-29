96b60d2efccbeda30445ef3be238d5243a6b98b0
wraikny <writtenknown@gmail.com>
2020-11-29 12:38:39 +0900

    update

--- generators/layout.fsx
+++ generators/layout.fsx
@@ -12,39 +12,52 @@ let injectWebsocketCode (webpage:string) =
     let websocketScript =
         """
         <script type="text/javascript">
-          var wsUri = "ws://localhost:8080/websocket";
-      function init()
-      {
-        websocket = new WebSocket(wsUri);
-        websocket.onclose = function(evt) { onClose(evt) };
-      }
-      function onClose(evt)
-      {
-        console.log('closing');
-        websocket.close();
-        document.location.reload();
-      }
-      window.addEventListener("load", init, false);
-      </script>
+            var wsUri = "ws://localhost:8080/websocket";
+            function init()
+            {
+              websocket = new WebSocket(wsUri);
+              websocket.onclose = function(evt) { onClose(evt) };
+            }
+            function onClose(evt)
+            {
+              console.log('closing');
+              websocket.close();
+              document.location.reload();
+            }
+            window.addEventListener("load", init, false);
+        </script>
         """
     let head = "<head>"
     let index = webpage.IndexOf head
     webpage.Insert ( (index + head.Length + 1),websocketScript)
 
+let tweetButton =
+    div [] [
+        a [ Href @"https://twitter.com/share?ref_src=twsrc%5Etfw"
+            Class "twitter-share-button"
+            Custom ("data-show-count", "false")
+        ] [ !! "Tweet" ]
+        script [ Async true; Src "https://platform.twitter.com/widgets.js"; CharSet "utf-8" ] []
+    ]
+
 let layout (ctx : SiteContents) active bodyCnt =
     let pages = ctx.TryGetValues<Pageloader.Page> () |> Option.defaultValue Seq.empty
     let siteInfo = ctx.TryGetValue<Globalloader.SiteInfo> ()
     let ttl =
-      siteInfo
-      |> Option.map (fun si -> si.title)
-      |> Option.defaultValue ""
+        siteInfo
+        |> Option.map (fun si ->
+            if active <> null && active <> ""
+            then sprintf "%s - %s" active si.title
+            else si.title
+        )
+        |> Option.defaultValue ""
 
     let menuEntries =
-      pages
-      |> Seq.map (fun p ->
-        let cls = if p.title = active then "navbar-item is-active" else "navbar-item"
-        a [Class cls; Href p.link] [!! p.title ])
-      |> Seq.toList
+        pages
+        |> Seq.map (fun p ->
+            let cls = if p.title = active then "navbar-item is-active" else "navbar-item"
+            a [Class cls; Href p.link] [!! p.title ])
+        |> Seq.toList
 
     html [] [
         head [] [
@@ -80,29 +93,31 @@ let layout (ctx : SiteContents) active bodyCnt =
     ]
 
 let render (ctx : SiteContents) cnt =
-  let disableLiveRefresh = ctx.TryGetValue<Postloader.PostConfig> () |> Option.map (fun n -> n.disableLiveRefresh) |> Option.defaultValue false
+    let disableLiveRefresh = ctx.TryGetValue<Postloader.PostConfig> () |> Option.map (fun n -> n.disableLiveRefresh) |> Option.defaultValue false
 
-  cnt
-  |> HtmlElement.ToString
-  |> fun n -> if disableLiveRefresh then n else injectWebsocketCode n
+    cnt
+    |> HtmlElement.ToString
+    |> fun n -> if disableLiveRefresh then n else injectWebsocketCode n
 
 let published (post: Postloader.Post) =
     post.published
     |> Option.defaultValue System.DateTime.Now
     |> fun n -> n.ToString("yyyy-MM-dd")
 
+
+
 let postLayout (useSummary: bool) (post: Postloader.Post) =
     div [Class "card article"] [
         match post.thumbnail with
         | Some path ->
-            yield div [Class "card-image"] [
+            div [Class "card-image"] [
                 let thumbnail = image [Src path] []
 
                 if useSummary then a [Href post.link] [thumbnail] else thumbnail
             ]
         | None -> ()
 
-        yield div [Class "card-content"] [
+        div [Class "card-content"] [
             div [Class "media-content has-image-centered"] [
                 p [Class "title article-title"; ] [ a [Href post.link] [!! post.title]]
                 p [Class "subtitle is-6 article-subtitle"] [
@@ -114,6 +129,11 @@ let postLayout (useSummary: bool) (post: Postloader.Post) =
                 !! (if useSummary then post.summary else post.content)
 
             ]
+
+            if not useSummary then
+                div [Class "content article-footer"] [
+                    tweetButton
+                ]
         ]
     ]
 
